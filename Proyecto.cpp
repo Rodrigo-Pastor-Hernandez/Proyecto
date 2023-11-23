03a3e559f2b902c14c6d235cc4f721ed63b58cbd
Rodrigo-Pastor-Hernandez <rodricool0212[200~@~gmail.com>
2023-11-22 22:28:47 -0600

    En este tercer commit le agrego un do while a cada caso del switch para que al igual que en el switch principal si pone una opcion que no este en el switch se repita hasta que ingrese el correcto igual sigue vacio y siemple ya que solo estoy viendo como queda para ya al final agregar todos los datos

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -5,6 +5,7 @@ using namespace std;
 main()
 {
 	int opcion;
+	
 	do{
 		cout<<"hola introduzca que opcion desea para continuar la hitoria"<<endl;
 	cin>>opcion;
@@ -13,6 +14,8 @@ main()
 			case 1:
 				cout<<"hola opcion 1"<<endl;
 					int opcion;
+					do
+					{
 					cout<<"hola segundo dentro opcion 1"<<endl;
 					cin>>opcion;
 						switch(opcion)
@@ -27,10 +30,14 @@ main()
 								cout<<"hola 3 dentro opcion 1";
 							break;
 						}
+					}while(opcion != 1 && opcion != 2 && opcion != 3);
+					
 			break;
 			case 2:
 					cout<<"hola opcion 2"<<endl;
 					int opcion2;
+					do
+					{
 					cout<<"hola segundo dentro opcion 2"<<endl;
 					cin>>opcion2;
 						switch(opcion2)
@@ -45,10 +52,14 @@ main()
 								cout<<"hola 3 dentro opcion 2";
 							break;
 						}
+					}while(opcion2 != 1 && opcion2 != 2 && opcion2 != 3);
+					
 			break;
 			case 3:
 					cout<<"hola opcion 3"<<endl;
 					int opcion3;
+					do
+					{
 					cout<<"hola segundo dentro opcion 3"<<endl;
 					cin>>opcion3;
 						switch(opcion3)
@@ -63,10 +74,35 @@ main()
 								cout<<"hola 3 dentro opcion 3";
 							break;
 						}
+					}while(opcion3 != 1 && opcion3 != 2 && opcion3 != 3);
+					
+			break;
+			case 4:
+					cout<<"hola opcion 4"<<endl;
+					int opcion4;
+					do
+					{
+						cout<<"hola segundo dentro opcion 4"<<endl;
+						cin>>opcion4;
+							switch(opcion4)
+							{
+								case 1:
+									cout<<"hola 1 dentro opcion 4";
+								break;
+								case 2:
+									cout<<"hola 2 dentro opcion 4";
+									break;
+								case 3:
+									cout<<"hola 3 dentro opcion 4";
+								break;
+							}
+					}while(opcion4 != 1 && opcion4 != 2 && opcion4 != 3);
 			break;
 		}	
-	}while(opcion != 1 && opcion != 2 && opcion != 3);
-	cout<<"hola introduce una opcion adecuada"<<endl;	
+		
+			cout<<"hola introduce una opcion adecuada"<<endl;
+			
+	}while(opcion != 1 && opcion != 2 && opcion != 3 && opcion !=4);
 	
 }	
 	
