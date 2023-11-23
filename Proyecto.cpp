eeda2276c2886ebeb2ca37755ba9d82b689705b2
Rodrigo-Pastor-Hernandez <rodricool0212[200~@~gmail.com>
2023-11-22 20:59:49 -0600

    En este segundo commit aunque no se vean muchos cambios yo le agrege la parte en la cual en caso de poner una opcion que no he pedido vuelta a dar la intruccion de pedir de nuevo las opciones hasta quer de una valida con ayuda de un do while

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -2,68 +2,71 @@
 
 using namespace std;
 
-main(){
-	
+main()
+{
 	int opcion;
-	
-	cout<<"hola"<<endl;
+	do{
+		cout<<"hola introduzca que opcion desea para continuar la hitoria"<<endl;
 	cin>>opcion;
-	
 	switch(opcion)
-	{
-		case 1:
-			cout<<"hola opcion 1"<<endl;
-				int opcion;
-				cout<<"hola segundo dentro opcion 1"<<endl;
-				cin>>opcion;
-					switch(opcion)
-					{
-						case 1:
-							cout<<"hola 1 dentro opcion 1";
-						break;
-						case 2:
-							cout<<"hola 2 dentro opcion 1";
+		{
+			case 1:
+				cout<<"hola opcion 1"<<endl;
+					int opcion;
+					cout<<"hola segundo dentro opcion 1"<<endl;
+					cin>>opcion;
+						switch(opcion)
+						{
+							case 1:
+								cout<<"hola 1 dentro opcion 1";
 							break;
-						case 3:
-							cout<<"hola 3 dentro opcion 1";
-						break;
-					}
-			break;
-		case 2:
-				cout<<"hola opcion 2"<<endl;
-				int opcion2;
-				cout<<"hola segundo dentro opcion 2"<<endl;
-				cin>>opcion2;
-					switch(opcion2)
-					{
-						case 1:
-							cout<<"hola 1 dentro opcion 2";
-						break;
-						case 2:
-							cout<<"hola 2 dentro opcion 2";
+							case 2:
+								cout<<"hola 2 dentro opcion 1";
+								break;
+							case 3:
+								cout<<"hola 3 dentro opcion 1";
 							break;
-						case 3:
-							cout<<"hola 3 dentro opcion 2";
-						break;
-					}
+						}
 			break;
-		case 3:
-				cout<<"hola opcion 3"<<endl;
-				int opcion3;
-				cout<<"hola segundo dentro opcion 3"<<endl;
-				cin>>opcion3;
-					switch(opcion3)
-					{
-						case 1:
-							cout<<"hola 1 dentro opcion 3";
-						break;
-						case 2:
-							cout<<"hola 2 dentro opcion 3";
+			case 2:
+					cout<<"hola opcion 2"<<endl;
+					int opcion2;
+					cout<<"hola segundo dentro opcion 2"<<endl;
+					cin>>opcion2;
+						switch(opcion2)
+						{
+							case 1:
+								cout<<"hola 1 dentro opcion 2";
 							break;
-						case 3:
-							cout<<"hola 3 dentro opcion 3";
-						break;
-					}
+							case 2:
+								cout<<"hola 2 dentro opcion 2";
+								break;
+							case 3:
+								cout<<"hola 3 dentro opcion 2";
+							break;
+						}
 			break;
-	}
-}
+			case 3:
+					cout<<"hola opcion 3"<<endl;
+					int opcion3;
+					cout<<"hola segundo dentro opcion 3"<<endl;
+					cin>>opcion3;
+						switch(opcion3)
+						{
+							case 1:
+								cout<<"hola 1 dentro opcion 3";
+							break;
+							case 2:
+								cout<<"hola 2 dentro opcion 3";
+								break;
+							case 3:
+								cout<<"hola 3 dentro opcion 3";
+							break;
+						}
+			break;
+		}	
+	}while(opcion != 1 && opcion != 2 && opcion != 3);
+	cout<<"hola introduce una opcion adecuada"<<endl;	
+	
+}	
+	
